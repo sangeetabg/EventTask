fc86e53c5aec9778ac8290337461bf7eaefc2802
sangeetabg <sangeeta.b.guttedar04@gmail.com>
2021-11-22 12:04:00 +0530

    ok

---
+++ first.js
@@ -0,0 +1,44 @@
+
+        const p1 = document.getElementById("btn1");
+       p1.addEventListener ("mouseover",function()
+         {
+             p1.style.backgroundColor = "green";
+         },true);
+
+         const p2 = document.getElementById("btn2");
+       p2.addEventListener ("mouseout",function()
+         {
+             p2.style.backgroundColor = "orange";
+         },true);
+
+
+         const p3 = document.getElementById("btn3");
+       const over3 =function()
+         {
+             p3.style.backgroundColor = "blue";
+         };
+
+         const p4 = document.getElementById("btn4");
+       const over4 =function()
+         {
+             p4.style.backgroundColor = "yellow";
+         };
+
+
+         const p5 = document.getElementById("btn5");
+         const over5 =function()
+
+         {
+             p5.style.backgroundColor = "brown";
+         };
+
+
+
+         const p6 = document.getElementById("btn6");
+       const over6 =function()
+         {
+             p6.style.backgroundColor = "grey";
+         };
+
+
+    
